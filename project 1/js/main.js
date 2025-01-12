a5e32fbc7737a76a5167867ba9dbf5ac356c47e9
Andy <115085994+VrMoore@users.noreply.github.com>
2025-01-12 22:06:12 +0700

    updated dashboard
    added local storage and updated about & dashboard pages

---
+++ project 1/js/main.js
@@ -0,0 +1,13 @@
+import StorageAPI from "../js/storage.js"
+import NoteViews from "../js/noteView.jss"
+
+
+// StorageAPI.saveNotes({
+//     id : 206,
+//     title : "Test 2",
+//     body : "Hello World 2"
+// });
+
+const app = document.getElementById()
+
+console.log(StorageAPI.getAllNotes())
---
+++ project 1/js/noteView.js
@@ -0,0 +1,5 @@
+export default class NotesView {
+    constructor(root, { noteSelect, noteAdd, noteEdit, noteDelete} = {}) {
+        
+    }
+}
