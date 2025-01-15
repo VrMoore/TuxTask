060599f5b8a0658b6f80bdbc7c3ec09023eedea5
Andy <115085994+VrMoore@users.noreply.github.com>
2025-01-15 21:52:08 +0700

    some fixes & updated js code

--- project 1/js/main.js
+++ project 1/js/main.js
@@ -6,7 +6,11 @@ import NoteViews from "../js/noteView.js"
 const app = document.getElementById('app');
 const view = new NoteViews(app, {
     noteAdd() {
-        console.log('note has been selected');
+        console.log("Let's add new notes");
+    },
+    noteEdit(newTitle, newBody) {
+        console.log(newTitle);
+        console.log(newBody);
     }
 });
 
--- project 1/js/noteView.js
+++ project 1/js/noteView.js
@@ -16,20 +16,52 @@ export default class NotesView {
                 </div>
             </aside>
             <article>
-                <h1>My Notes</h1>
+                <input type="text" id="notes_title" placeholder="New Title" spellcheck="false"></input>
                 <section>
-                    <input type="text" id="notes_title" placeholder="New Title"></input>
-                    <textarea class="notes_body" placeholder="add text...."></textarea>
+                    <textarea class="notes_body" spellcheck="false">Add texts</textarea>
                 </section>
             </article>
         `;
 
-        const btnAddNote = this.root.querySelector("notes_add");
-        const titleNote = this.root.querySelector("notes_title");
-        const bodyNotes = this.root.querySelector("notes_body");
+        const btnAddNote = this.root.querySelector(".notes_add");
+        const titleNotes = this.root.querySelector(".notes_title");
+        const bodyNotes = this.root.querySelector(".notes_body");
 
         btnAddNote.addEventListener("click", () => {
             this.noteAdd();
         }); 
+
+        if (titleNotes && bodyNotes) {
+
+            [titleNotes, bodyNotes].forEach(inputField => {
+                inputField.addEventListener("blur", () => {
+                    const updatedTitle = titleNotes.value.trim();
+                    const updatedBody = bodyNotes.value.trim();
+    
+                    this.noteEdit(updatedTitle, updatedBody);
+                });
+            });
+        } else {
+            console.error("Error")
+        }
+
+        console.log(this._createListItem(200, "Notes 1", "Lorem Ipsum dolor sit amet"))
+
     };
+
+    _createListItem(id, title, body) {
+        const MAX_BODY_LENGTH = 60;
+
+        return `
+            <div class="note_list_items" data-node-id="${id}">
+            
+                <div class="notes_small_title">${title}</div>
+                <div class="notes_small_body">
+                    ${body.substring(0, MAX_BODY_LENGTH)}
+                    ${body.length > MAX_BODY_LENGTH ? "...." : ""}
+                </div>
+            
+            </div>
+        `;
+    }
 };
