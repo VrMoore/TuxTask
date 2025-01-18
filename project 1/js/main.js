45c6e9fbe36d1a46dd69b18c31619e21085a497a
Andy <115085994+VrMoore@users.noreply.github.com>
2025-01-18 22:05:47 +0700

    updted dasboard

--- project 1/js/main.js
+++ project 1/js/main.js
@@ -1,20 +1,6 @@
-import StorageAPI from "../js/storage.js"
-import NoteViews from "../js/noteView.js"
+import App from './apps.js';
+
+const root = document.getElementById("app");
+const app = new App(root);
 
 
-
-const app = document.getElementById('app');
-const view = new NoteViews(app, {
-    noteAdd() {
-        console.log("Let's add new notes");
-    },
-    noteSelect(id) {
-        console.log("Let's select a note with id: ", id)
-    },
-    noteEdit(newTitle, newBody) {
-        console.log(newTitle);
-        console.log(newBody);
-    }
-});
-
-view.updateNoteList(StorageAPI.getAllNotes());
--- project 1/js/noteView.js
+++ project 1/js/noteView.js
@@ -14,7 +14,7 @@ export default class NotesView {
                 </button>
                 <div class="notes_list"></div>
             </aside>
-            <article>
+            <article class="notes__preview">
                 <input type="text" id="notes__title" placeholder="New Title" spellcheck="false"></input>
                 <section>
                     <textarea class="notes__body" placeholder="add text...."  spellcheck="false"></textarea>
@@ -41,21 +41,21 @@ export default class NotesView {
             });
         };
 
-        console.log(this._createListItem(200, "Notes 1", "Lorem Ipsum dolor sit amet"))
-
+        this.updateNotePreviewVisibility(false);
+        
     };
 
     _createListItem(id, title, body) {
         const MAX_BODY_LENGTH = 60;
 
         return `
-            <div class="note_list_items" data-node-id="${id}">
+            <div class="notes_list_items" data-note-id="${id}">
             
-                <div class="notes_small_title">${title}</div>
-                <div class="notes_small_body">
+                <h3 class="notes_small_title">${title}</h3>
+                <p class="notes_small_body">
                     ${body.substring(0, MAX_BODY_LENGTH)}
                     ${body.length > MAX_BODY_LENGTH ? "...." : ""}
-                </div>
+                </p>
             
             </div>
         `;
@@ -72,11 +72,38 @@ export default class NotesView {
             notesList.insertAdjacentHTML('beforeend', html);
         }
 
-        notesList.querySelectorAll(".note_list_items").forEach(noteListItem => {
+        notesList.querySelectorAll(".notes_list_items").forEach(noteListItem => {
             noteListItem.addEventListener("click", () => {
                 this.noteSelect(noteListItem.dataset.noteId);
+            });
+
+            noteListItem.addEventListener("contextmenu", (e) => {
+                e.preventDefault();
+                this.noteDelete(noteListItem.dataset.noteId);
+            });
+
+            noteListItem.addEventListener("dblclick", () => {
+                const doDelete = confirm("Are you sure to DELETE this note?");
+
+                if (doDelete) {
+                    this.noteDelete(noteListItem.dataset.noteId)
+                }
             });
         });
     }
 
+    updateActiveNote(note) {
+        this.root.querySelector(".notes__title").value = note.title;
+        this.root.querySelector(".notes__body").value = note.body;
+
+        this.root.querySelectorAll(".notes_list_items").forEach(noteListItem => {
+            noteListItem.classList.remove("notes_list_items--selected");
+            
+        })  
+        this.root.querySelector(`.notes_list_items[data-note-id="${note.id}"]`).classList.add("notes_list_items--selected"); 
+    }
+
+    updateNotePreviewVisibility(visible) {
+        this.root.querySelector(".notes__preview").style.visibility = visible ? "visible" : "hidden";
+    }
 };
