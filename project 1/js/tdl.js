050efbebf1c693bd1315a15f680d6a5d766f7d48
Andy <115085994+VrMoore@users.noreply.github.com>
2025-01-25 22:15:00 +0700

    Updated Task List
    Make functional list and make new save local (still bugged)

--- project 1/js/tdl.js
+++ project 1/js/tdl.js
@@ -15,27 +15,74 @@ listItemsElement.forEach(item => {
         crossX.style.transform = '';
         crossX.style.transition = '.5s all ease';
     });
+
 });
 
-const addBtn = document.getElementById('add_btn');
+const addBtn = document.getElementById('input_button');
 const inputBox = document.getElementById('input_task');
-
-console.log(inputBox);
-console.log(addBtn);
 
 function addTask() {
     
     addBtn.addEventListener('click', () => {
         let li = document.createElement("li");
-        li.classList.add('items_list');
-        li.innerHMTL = `
-            <input type="checkbox" id="check">
-            <p>${inputBox}</p>
-            <i id="cross" class="fa-solid fa-xmark"></i>
-        `;
+        li.classList.add('lists_items');
+        
+        createListItem(inputBox, li);
 
         listItems.appendChild(li);
+        console.log(li)
+    });
+
+    inputBox.value = "";
+    saveToLocal();
+
+}
+
+function createListItem(inputBox, li) {
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = 'check';
+    li.appendChild(checkbox);
+
+    const paragraph = document.createElement("p");
+    paragraph.classList.add('desc');
+    paragraph.textContent = inputBox.value;
+    li.appendChild(paragraph);
+    
+    const cross = document.createElement("i");
+    cross.classList.add('fa-solid','fa-xmark');
+    cross.id = 'cross';
+    li.appendChild(cross);
+
+    cross.addEventListener('click', () => {
+        li.remove()
+        saveToLocal();
     })
+
+    li.addEventListener('click', (e) => {
+        if (e.target != cross) {
+            checkbox.checked = !checkbox.checked;
+        }
+        saveToLocal();
+    })
+
+};
+
+function saveToLocal() {
+    const localData = [];
+    const listItemsContent = document.querySelectorAll(".items_list")
+
+    listItemsContent.forEach(item => {
+        const checkbox = item.querySelector('.check');
+        const desc = item.querySelector('.desc');
+
+        localData.push({
+            description : desc,
+            checked : checkbox.checked
+        })
+    })
+
+    localStorage.setItem('task list data', JSON.stringify(localData))
 }
 
 addTask();
